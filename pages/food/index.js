567b7a3a25f7d516f8ff48ec9a8de57c8fb4409e
abdulahhamad <alhmada176@gmail.com>
2021-12-31 08:47:19 +0300

    build up

--- pages/food/index.js
+++ pages/food/index.js
@@ -1,19 +1,21 @@
 import Link from "next/link";
 import { Card, Col, Container, Row } from "react-bootstrap";
 import Styles from "../../styles/FoodCart.module.scss";
+import { faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Foods({ food }) {
   return (
     <>
       <Container>
         <Row>
-          <Link href={"/"} variant="primary">
-            Back
+          <Link  href={"/foods"} variant="primary">
+            <FontAwesomeIcon className={Styles.backButton} icon={faArrowCircleLeft}  />
           </Link>
           <Col xs={12} key={food.id}>
-            <Card className={Styles.DetailsfoodCart}>
-              <Col md={6} className={Styles.image}>
+              <Col className={Styles.detailImage}>
                 <Card.Img variant="top" src={food.get_image} />
               </Col>
+            <Card className={Styles.DetailsfoodCart}>
               <Card.Body>
                 <Card.Title>{food.name}</Card.Title>
                 <Card.Text>{food.description}</Card.Text>
--- pages/foods.js
+++ pages/foods.js
@@ -8,8 +8,8 @@ export default function food({ foods }) {
     </>
   );
 }
-export const getStaticProps = async (res) => {
-  const res = await fetch(`https://mardood.pythonanywhere.com/api/v1.0/food`);
+export const getStaticProps = async () => {
+  const res = await fetch(`https://mardood.pythonanywhere.com/api/v1.0/food/`);
   const foods = await res.json();
   return {
     props: {
